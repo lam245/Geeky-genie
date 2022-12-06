d416b003d877996affe950e8517c15c2bd0fbe49
natalyst208 <tuandeptrailtt@gmail.com>
2022-12-06 11:06:17 +0700

    full author

--- src/scenes/Author.jsx
+++ src/scenes/Author.jsx
@@ -10,7 +10,6 @@ const Author = (props) => {
 
   const { auth_id } = useParams()
 
-
   useEffect(() => {
     axios.get(`http://w22g7.int3306.freeddns.org/authors/?author_id=${auth_id}`).then(res => {
       setUser(res.data)
@@ -36,10 +35,14 @@ const Author = (props) => {
             <div class="p-3">
               <div class="fw-bold text-center fs-3">Thông tin cá nhân</div>
               <div class="px-1 mt-3">
+                <div class="fs-6 fw-bold">Follower</div>
+                <div style={{ color: "#999" }}>{user.follower ?? "Không có thông tin"}</div>
+              </div>
+              <div class="fs-6 px-1 mt-3">
                 <div class="fw-bold">Website</div>
                 <div style={{ color: "#999" }}>{user.website ?? "Không có thông tin"}</div>
               </div>
-              <div class="px-1 mt-3">
+              <div class="fs-6 px-1 mt-3">
                 <div class="fw-bold">Email</div>
                 <div style={{ color: "#999" }}>{user.social_account ?? "Không có thông tin"} </div>
               </div>
@@ -48,59 +51,35 @@ const Author = (props) => {
           </div>
           <div class="col-12 col-md-9 text-white">
             <div class="fs-1 fw-bold">{user.author_name}</div>
-            <div class="fw-bold mt-4">Tiểu sử</div>
+            <div class="fs-3 fw-bold mt-4">Tiểu sử</div>
             <div style={{ color: "#999" }} class="mt-2">
               {user.bio  ?? "Không có thông tin"}
             </div>
-            <div class="fw-bold mt-4">Quotes</div>
+            <div class="fs-3 fw-bold mt-4">Quotes</div>
             <div style={{ color: "#999" }} class="mt-2">
               {user.quote  ?? "Không có thông tin"}
             </div>
-            <div class="fw-bold mt-4">Các tác phẩm</div>
+            <div class="fs-3 fw-bold mt-4">Các tác phẩm</div>
             <div class="row mt-2">
               {books.map((book) => (
               <div class="col-12 col-md-3">
-                <a href="" className='no-underline'>
+                <a className='no-underline book' href={`http://w22g7.int3306.freeddns.org/book/${book.book_id}`} target="_blank">
                   <img
                     src={book.cover}
-                    class="w-100 image-height"
+                    class="w-100 h-100"
                     alt="image"
-                  />
-                  <div style={{ color: "#999" }} class="text-center mt-2">
+                    />
+                    <div className='rating'>
+                    {/* http://w22g7.int3306.freeddns.org/book/book_id */}
                       <Rating name="half-rating" value={book.current_rating} precision={1} />
-                      <h1 className='text'>{book.title}</h1>
-                  </div>
+                    </div>
                 </a>
+                
+                      <h1 className='fs-4 fw-bold mt-4 text-center'>{book.title}</h1>
+              
               </div>
                 
               ))}
-           {/*    <div class="col-12 col-md-3">
-                <img
-                  src="https://taimienphi.vn/tmp/cf/aut/0jRR-odmp-K3i2-8y3Q-w1jw-aKXX-BfHP-hinh-nen-1.jpg"
-                  class="w-100"
-                />
-                <div style={{ color: "#999" }} class="text-center mt-2">
-                  Tác Phẩm 2
-                </div>
-              </div>
-              <div class="col-12 col-md-3">
-                <img
-                  src="https://taimienphi.vn/tmp/cf/aut/0jRR-odmp-K3i2-8y3Q-w1jw-aKXX-BfHP-hinh-nen-1.jpg"
-                  class="w-100"
-                />
-                <div style={{ color: "#999" }} class="text-center mt-2">
-                  Tác Phẩm 3
-                </div>
-              </div>
-              <div class="col-12 col-md-3">
-                <img
-                  src="https://taimienphi.vn/tmp/cf/aut/0jRR-odmp-K3i2-8y3Q-w1jw-aKXX-BfHP-hinh-nen-1.jpg"
-                  class="w-100"
-                />
-                <div style={{ color: "#999" }} class="text-center mt-2">
-                  Tác Phẩm 4
-                </div>
-              </div> */}
             </div>
           </div>
         </div>
